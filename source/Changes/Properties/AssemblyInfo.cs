982ff0bcb3b21a5f11639e80dfa96b032994b5a0
Chris Block <chris.block@gmail.com>
2012-06-05 13:55:05 -0500

    Created nuspec and started working on the build for this project.

--- source/Changes/Properties/AssemblyInfo.cs
+++ source/Changes/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Changes")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A DynamicObject based generic ChangeSet for managing partial changes to POCO objects supplied through JSON HTTP requests.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Changes")]
+[assembly: AssemblyCompany("Chris Block")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
